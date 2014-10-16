f8dab557dc78affc55c57a656bd4faca44ec1120
Joe <josef.erneker@gmail.com>
2014-10-16 16:14:50 +0200

    Chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  BASE_TITLE = 'Ruby on Rails Tutorial Sample App'
+
+  def full_title(specific_title)
+    if specific_title
+      "#{BASE_TITLE} | #{specific_title}"
+    else
+      BASE_TITLE
+    end
+  end
 end

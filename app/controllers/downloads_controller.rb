48207951159ac7307d2689957b0bb5cd3a0c4de4
Erneker Josef <Josef.Erneker@ysoft.com>
2015-07-31 23:48:29 +0200

    Fix DownloadsHelper import

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,6 +1,7 @@
 class DownloadsController < ApplicationController
-  PATH = 'public/assets/data'
+  include DownloadsHelper
 
+  PATH = 'public/assets/data'
   FILES_TO_DOWNLOAD.each { |file| define_method(file.gsub('.', '_')) { send_public_asset(file) } }
 
   def send_public_asset(file)

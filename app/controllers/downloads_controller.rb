28bb00ec9efb624711a6da9651881f0fccbabfff
Erneker Josef <Josef.Erneker@ysoft.com>
2015-09-07 00:52:36 +0200

    Remove duplicity

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -2,10 +2,5 @@ class DownloadsController < ApplicationController
   include DownloadsHelper
 
   PATH = 'public/assets/data'
-  FILES_TO_DOWNLOAD.each { |file| define_method(file.gsub('.', '_')) { send_public_asset(file) } }
-
-  def send_public_asset(file)
-    log_visit
-    send_file "#{PATH}/#{file}"
-  end
+  FILES_TO_DOWNLOAD.each { |file| define_method(file.gsub('.', '_')) { send_file "#{PATH}/#{file}" } }
 end

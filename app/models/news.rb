bad06740c7435deaa679d4237da2818c6420e7b8
Erneker Josef <Josef.Erneker@ysoft.com>
2015-10-10 16:26:27 +0200

    Remove collapsed from news and add mention that AnyBar is for Mac only

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,11 +1,10 @@
 class News < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
  # has_many :comments
+  before_create :collapse
 
   default_scope -> { order('created_at DESC') }
 
   validates :name, presence: true, length: {maximum: 50}
   validates :content, presence: true
-
-  attr_accessor :collapsed
 end

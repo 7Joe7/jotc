4be7b05e72d0040b4b10a130d17189e8d7b8ded5
Erneker Josef <Josef.Erneker@ysoft.com>
2015-10-30 13:33:30 +0100

    Fix creation of news through active admin

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,7 +1,6 @@
 class News < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
  # has_many :comments
-  before_create :collapse
 
   default_scope -> { order('created_at DESC') }
 
